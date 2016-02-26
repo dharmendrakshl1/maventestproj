653e0b4fd8df17eb68fde297f7278c7d786bc4a3
dharmendrakshl1 <dharmendra.kshl1@gmail.com>
2016-02-26 06:04:43 +0530

    Added files via upload
    Added

---
+++ src/com/main/MyClass.java
@@ -0,0 +1,8 @@
+package com.main;
+
+public class MyClass {
+	public void disp() {
+		// TODO Auto-generated method stub
+		System.out.println("Disp From MyClass");
+	}
+}
---
+++ src/com/test/Test.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import com.main.MyClass;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass mc = new MyClass();
+		mc.disp();
+	}
+
+}
